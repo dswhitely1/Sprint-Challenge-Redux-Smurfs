e86165e3cb1c6dc3280681b387ac735dd09ce08d
Donald Whitely <dswhitely1@gmail.com>
2019-06-21 13:05:28 -0400

    Edit Smurf form routing to '/' on Error

--- smurfs/src/components/SmurfForm.jsx
+++ smurfs/src/components/SmurfForm.jsx
@@ -1,24 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { addSmurf } from '../actions';
+import { addSmurf, updateSmurf } from '../actions';
 
 const SmurfForm       = props => {
-  const [name, setName] = useState(props.smurf.name);
-  const [age, setAge] = useState(props.smurf.age);
-  const [height, setHeight] = useState(props.smurf.height);
-  const handleSubmit = e => {
+  if (props.smurf === "Reroute") props.history.push('/');
+  const [name, setName]     = useState( props.smurf.name );
+  const [age, setAge]       = useState( props.smurf.age );
+  const [height, setHeight] = useState( props.smurf.height );
+  const [id, setId] = useState(props.smurf.id);
+  const handleSubmit        = e => {
     e.preventDefault();
-    props.addSmurf({name,age,height});
-    if (!props.error) props.history.push('/');
-  }
+
+    props.addSmurf( {
+      name,
+      age,
+      height
+    } );
+    if (!props.error) props.history.push( '/' );
+  };
   return (
     <div>
       <h2 className='text-center'>{`Add / Update Your Chosen Smurf`}</h2>
-      <form onSubmit={e=>handleSubmit(e)}>
+      <form onSubmit={e => handleSubmit( e )}>
         <div className='form-group'>
           <label className='mr-1' htmlFor='name'>Smurf Name: </label>
           <input
-            onChange={e=>setName(e.target.value)}
+            onChange={e => setName( e.target.value )}
             placeholder="name"
             value={name}
             name="name"
@@ -29,7 +36,7 @@ const SmurfForm       = props => {
         <div className='form-group'>
           <label htmlFor='age'>Smurf Age:</label>
           <input
-            onChange={e=>setAge(e.target.value)}
+            onChange={e => setAge( e.target.value )}
             placeholder="age"
             value={age}
             name="age"
@@ -40,7 +47,7 @@ const SmurfForm       = props => {
         <div className='form-group'>
           <label htmlFor='height'>Smurf Height</label>
           <input
-            onChange={e=>setHeight(e.target.value)}
+            onChange={e => setHeight( e.target.value )}
             placeholder="height"
             value={height}
             name="height"
@@ -49,20 +56,23 @@ const SmurfForm       = props => {
           />
         </div>
         <button className='btn btn-success'
-                type="submit">Submit</button>
+                type="submit">Submit
+        </button>
       </form>
     </div>);
 };
 const mapStateToProps = ( state, ownProps ) => {
-  const editSmurf = ownProps.match.params.id ? state.smurfs.filter( smurf => smurf.id.toString() === ownProps.match.params.smurfId )[0] : ({
+  const editSmurf = ownProps.match.params.smurfId ? state.smurfs.filter( smurf => smurf.id.toString() === ownProps.match.params.smurfId )[0] : ({
     name  : '',
     age   : '',
-    height: ''
+    height: '',
+    id: null
   });
+
   return {
-    smurf: editSmurf,
+    smurf: editSmurf===undefined ? 'Reroute': editSmurf,
     error: state.error
   };
 };
 
-export default connect( mapStateToProps, {addSmurf} )( SmurfForm );
+export default connect( mapStateToProps, {addSmurf, updateSmurf} )( SmurfForm );
