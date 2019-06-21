8460e50788c2e32449c31c346437fabbe018f319
Donald Whitely <dswhitely1@gmail.com>
2019-06-21 11:16:35 -0400

    Implemented the Redux DevTools Extension

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers';
-
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
-);
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById( 'root' )
 );
--- smurfs/src/store.js
+++ smurfs/src/store.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers'
 
-export default (createStore(rootReducer,applyMiddleware(thunk, logger)));
+export default (createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk, logger))));
