fce432bd8baa8f04d1c6eed9afc9b661dd416339
Donald Whitely <dswhitely1@gmail.com>
2019-06-21 11:10:36 -0400

    Skelton Reducer created

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import C from '../actions/types';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,7 +14,11 @@
    error: null
  }
 */
-
+const initialState = {
+  smurfs: [],
+  isLoading: false,
+  error: null
+}
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +26,11 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (state=initialState, action) =>{
+  const {type, payload} = action;
+  switch (type) {
+    default:
+      return state;
+  }
+}
