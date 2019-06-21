8cd35f93c1891c587871562c07b55b2baab3a50e
Donald Whitely <dswhitely1@gmail.com>
2019-06-21 11:20:15 -0400

    Fetch Smurf action creator created

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,8 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import C from './types';
+import axios from 'axios';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +14,13 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchSmurfs = () => async dispatch => {
+  dispatch( {type: C.FETCH_SMURF_START} );
+  try {
+    const res = await axios.get( 'http://localhost:3333/smurfs' );
+    console.log( res );
+  } catch (error) {
+    console.log( error );
+  }
+};
